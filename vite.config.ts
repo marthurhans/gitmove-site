e79423695c6b4a8945bf4906e95df45215227bce
Mike Hans <marthurhans@gmail.com>
2025-06-19 21:47:23 -0400

    Fix: Include Netlify _redirects via Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { readFileSync, writeFileSync } from 'fs';
+
+const copyRedirects = () => {
+  return {
+    name: 'copy-redirects',
+    writeBundle() {
+      const src = resolve(__dirname, 'public/_redirects');
+      const dest = resolve(__dirname, 'dist/_redirects');
+      const content = readFileSync(src);
+      writeFileSync(dest, content);
+    },
+  };
+};
+
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), copyRedirects()]
+});
